8e5e02b6eaac15d8b7dd743a33d29cc7daf2c0b7
leoportogtr86 <leoporto_studio@hotmail.com>
2022-05-07 07:42:54 -0300

    arquivos estaticos

--- src/app.js
+++ src/app.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     return res.json({ msg: 'Ok' });
 })
 
-app.get('/p1', (req, res) => {
+app.get('/p1/:nome/:idade/:profissao', (req, res) => {
     let condicao = true;
     let titulo = condicao ? 'Título 1' : 'Título 2';
-    res.render('p1', { titulo });
+    res.render('p1',
+        {
+            titulo,
+            nome: req.params.nome,
+            idade: req.params.idade,
+            profissao: req.params.profissao
+        });
 })
 
 app.listen(8080, () => {
